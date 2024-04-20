45576ca884b03110c3d8f264dd256f795baf9803
Mariana <marian.muniz.rocha@gmail.com>
2024-04-20 12:51:57 -0300

    Projeto Operações Finalizado

--- Operacoes/Operacoes.py
+++ Operacoes/Operacoes.py
@@ -1,8 +1,5 @@
-# Projeto Operações
+#Ler dois números inteiros, executar e mostrar o resultado das seguintes operações: adição, subtração, multiplicação e divisão.
 
-Ler dois números inteiros, executar e mostrar o resultado das seguintes operações: adição, subtração, multiplicação e divisão.
-
-```py
 num1 = int(input('Digite o primeiro número: '))
 num2 = int(input('Digite o segundo número: '))
 
@@ -10,4 +7,3 @@ print(f'A soma dos dois números é {num1 + num2}')
 print(f'A subtração dos dois números é {num1 - num2}')
 print(f'A multiplicação dos dois números é {num1 * num2}')
 print(f'A divisão dos dois números é {num1 % num2}')
-```
